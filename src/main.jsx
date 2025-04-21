dedc7f16dc777bf8bed9ecf6fa0513931247fbf0
Nathaniel Thomas <nathaniel.thomas.2004@gmail.com>
2025-04-21 15:25:04 -0500

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import App from './App.jsx'
+import { inject } from '@vercel/analytics';
+
+inject();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
