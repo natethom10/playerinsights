8aaa240d563bbe9e421ab071b754b093e35c573d
Nathaniel Thomas <nathaniel.thomas.2004@gmail.com>
2025-04-24 13:18:32 -0500

    sync with class

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,33 @@ import Lines from "./frontend/components/Lines";
 import Login from "./frontend/components/Login";
 import Builder from "./frontend/components/Builder";
 import Home from "./frontend/components/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  const [step, setStep] = useState("home");
   const [league, setLeague] = useState("nba");
 
   return (
     <>
-      <Navigation setStep={setStep} league={league} setLeague={setLeague} />
+      {/* <Navigation setStep={setStep} league={league} setLeague={setLeague} />
       <div className="p-3 text-light">
         {step === "home" && <Home />}
         {step === "recents" && <Recents />}
-        {step === "lines" && <Lines league={league}/>}
+        {step === "lines" && <Lines league={league} />}
         {step === "builder" && <Builder />}
         {step === "login" && <Login />}
-      </div>
+      </div> */}
+      <Router>
+        <Navigation league={league} setLeague={setLeague} />
+        <div className="p-3 text-light">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/:league/recents" element={<Recents />} />
+            <Route path="/:league/lines" element={<Lines league={league} />} />
+            <Route path="/builder" element={<Builder league={league} />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 };
--- src/frontend/components/Navigation.jsx
+++ src/frontend/components/Navigation.jsx
@@ -3,33 +3,50 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Dropdown from "react-bootstrap/Dropdown";
+import { Link, useNavigate } from "react-router-dom";
 
-const Navigation = ({ setStep, league, setLeague }) => {
+const Navigation = ({ league, setLeague }) => {
+  const navigate = useNavigate();
+
+  const handleLeagueChange = (newLeague) => {
+    setLeague(newLeague);
+    navigate(`${newLeague}/lines`);
+  };
   return (
     <div>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container fluid className="d-flex">
           <div className="d-flex">
-            <Navbar.Brand onClick={() => setStep("home")} className="homepage-name">
-              PlayerInsights
+            <Navbar.Brand className="homepage-name">
+              <Link to="/" className="text-decoration-none text-white">PlayerInsights</Link>
             </Navbar.Brand>
             <Dropdown>
-              <Dropdown.Toggle>{league !== "League" ? league.toUpperCase() : league}</Dropdown.Toggle>
+              <Dropdown.Toggle>
+                {league !== "League" ? league.toUpperCase() : league}
+              </Dropdown.Toggle>
               <Dropdown.Menu>
-                <Dropdown.Item onClick={() => setLeague("nba")}>
+                <Dropdown.Item onClick={() => handleLeagueChange("nba")}>
                   NBA
                 </Dropdown.Item>
-                <Dropdown.Item onClick={() => setLeague("mlb")}>
+                <Dropdown.Item onClick={() => handleLeagueChange("mlb")}>
                   MLB
                 </Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
           </div>
           <Nav>
-            <Nav.Link onClick={() => setStep("recents")}>Recents</Nav.Link>
-            <Nav.Link onClick={() => setStep("lines")}>Lines</Nav.Link>
-            <Nav.Link onClick={() => setStep("builder")}>Builder</Nav.Link>
-            <Nav.Link onClick={() => setStep("login")}>Login</Nav.Link>
+            <Nav.Link as={Link} to={`/${league}/recents`}>
+              Recents
+            </Nav.Link>
+            <Nav.Link as={Link} to={`/${league}/lines`}>
+              Lines
+            </Nav.Link>
+            <Nav.Link as={Link} to="/builder">
+              Builder
+            </Nav.Link>
+            <Nav.Link as={Link} to="/login">
+              Login
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
